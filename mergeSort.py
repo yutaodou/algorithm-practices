9f24bf0494ab4b073c6d231d5a4d3008e539aeaf
Dou Yutao <yutao.dou@rea-group.com>
2016-11-15 23:28:22 +0800

    pythonic

--- mergeSort.py
+++ mergeSort.py
@@ -1,33 +1,32 @@
-#!/use/bin/env python
-
-
 class MergeSort(object):
 
     def sort(self, arr):
-        self.mergeSort(arr, 0, len(arr))
+        def mergeSort(arr, start, end):
+            if end - start < 2:
+                return
+
+            middle = (start + end) / 2
+            mergeSort(arr, start, middle)
+            mergeSort(arr, middle, end)
+            merge(arr, start, middle, end)
+
+        def merge(arr, start, middle, end):
+            working = [x for x in arr]
+
+            i = start
+            j = middle
+            for k in range(start, end):
+                if(i < middle and (j >= end or working[i] <= working[j])):
+                    arr[k] = working[i]
+                    i = i + 1
+                else:
+                    arr[k] = working[j]
+                    j = j + 1
+
+        mergeSort(arr, 0, len(arr))
         return arr
 
-    def mergeSort(self, arr, start, end):
-        if end - start < 2:
-            return
 
-        middle = (start + end) / 2
-        self.mergeSort(arr, start, middle)
-        self.mergeSort(arr, middle, end)
-        self.merge(arr, start, middle, end)
-
-    def merge(self, arr, start, middle, end):
-        working = [x for x in arr]
-
-        i = start
-        j = middle
-        for k in range(start, end):
-            if(i < middle and (j >= end or working[i] <= working[j])):
-                arr[k] = working[i]
-                i = i + 1
-            else:
-                arr[k] = working[j]
-                j = j + 1
 
 arr = [1, 3, 2, 4, 5, 7, 1, 2, 100, 20, 19]
 print MergeSort().sort(arr)
--- quickSort.py
+++ quickSort.py
@@ -1,36 +1,33 @@
-#!/usr/bin/env python
-
-
 class QuickSort(object):
 
     def sort(self, arr):
-        self.quickSort(arr, 0, len(arr) - 1)
+        def quickSort(arr, start, end):
+            if start < end:
+                idx = partition(arr, start, end)
+                if idx > start:
+                    quickSort(arr, start, idx - 1)
+                if idx < end:
+                    quickSort(arr, idx + 1, end)
+
+        def swap(arr, i, j):
+            tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+
+        def partition(arr, start, end):
+            pivot = arr[end]
+            idx = start
+            for j in range(start, end):
+                if arr[j] <= pivot:
+                    swap(arr, idx, j)
+                    idx += 1
+
+            swap(arr, idx, end)
+            return idx
+
+        quickSort(arr, 0, len(arr) - 1)
         return arr
 
-    def quickSort(self, arr, start, end):
-        if start < end:
-            idx = self.partition(arr, start, end)
-
-            if idx > start:
-                self.quickSort(arr, start, idx - 1)
-            if idx < end:
-                self.quickSort(arr, idx + 1, end)
-
-    def partition(self, arr, start, end):
-        pivot = arr[end]
-        idx = start
-
-        for j in range(start, end):
-            if arr[j] <= pivot:
-                tmp = arr[idx]
-                arr[idx] = arr[j]
-                arr[j] = tmp
-                idx += 1
-
-        arr[end] = arr[idx]
-        arr[idx] = pivot
-
-        return idx
 
 arr = [1, 3, 2, 4, 5, 7, 1, 2, 100, 9, 20, 19]
 print QuickSort().sort(arr)
